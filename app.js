a0bf3df125dde69eb2a935bf1f07ae8d278cbd6f
Sam Da Silva Loureiro <sam.dasilvaloureiro@heig-vd.ch>
2023-02-01 16:44:06 +0100

    final code

--- app.js
+++ app.js
@@ -1,1 +1,45 @@
 "use strict";
+
+const stationName = document.querySelector(".title h1");
+
+const boardContainer = document.querySelector("#board");
+
+const getStations = async (station) => {
+    try {
+        const res = await fetch(`http://transport.opendata.ch/v1/stationboard?station=${station}&limit=10`);
+        const data = await res.json();
+        console.log(data);
+        afficheNomStation(data);
+        getAllStationboard(data);
+    } catch (e) {
+        console.error(e.message);
+    }
+}
+
+const addStationboard = (stationboard) => {
+    const category = stationboard.category;
+    const name = stationboard.to;
+    const date = new Date(stationboard.stop.departure);
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    const html = `<article>
+    <div class="time">${hours}:${minutes}</div>
+    <div class="category" data-category="${category}">${category}</div>
+    <div class="destination">${name}</div>
+</article>`; 
+    boardContainer.insertAdjacentHTML("beforeend", html)
+}
+
+const afficheNomStation = (data) => {
+    const dataStationName = data.station.name;
+    stationName.textContent = "🚆 " + dataStationName;
+}
+
+const getAllStationboard = (data) => {
+    const allStationboard = data.stationboard;
+    allStationboard.forEach(e => {
+        addStationboard(e);
+    });
+}
+
+getStations("Yverdon-les-Bains");
